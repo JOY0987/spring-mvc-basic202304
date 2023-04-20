b2e52845314fcc1165eeaf92bc08a6fd5f797c0a
JOY0987 <jjoyewon@gmail.com>
2023-04-20 12:25:11 +0900

    Score - save

---
+++ src/main/java/com/spring/mvc/chap04/dto/ScoreRequestDTO.java
@@ -0,0 +1,16 @@
+package com.spring.mvc.chap04.dto;
+
+import lombok.*;
+
+// DTO 는 Setter, NoArgsConstructor 필수
+@Setter @Getter
+@ToString
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode
+public class ScoreRequestDTO {
+
+    // 필드명은 input 태그 내 name 과 똑같게 설정
+    private String name; // 학생 이름
+    private int kor, eng, math; // 국, 영, 수 점수
+}
